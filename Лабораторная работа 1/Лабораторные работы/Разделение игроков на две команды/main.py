391826737d2058ed89b6cf8dae678100d0a360d5
KOST9K <99390244+KOST9K@users.noreply.github.com>
2023-10-15 11:38:34 +0300

    Update main.py

--- Лабораторная работа 1/Лабораторные работы/Разделение игроков на две команды/main.py
+++ Лабораторная работа 1/Лабораторные работы/Разделение игроков на две команды/main.py
@@ -1,6 +1,6 @@
 list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
 
-team_size = int(len(list_players)/2)
+team_size = len(list_players) // 2
 first_team = list_players[:team_size]
 second_team = list_players[team_size::]
 
