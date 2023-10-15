e7dbc88c04ca74ee01b811067f81b803783d7c35
KOST9K <99390244+KOST9K@users.noreply.github.com>
2023-10-15 11:35:57 +0300

    Update main.py

--- Лабораторная работа 1/Лабораторные работы/Замена пропущенного элемента средним арифметическим/main.py
+++ Лабораторная работа 1/Лабораторные работы/Замена пропущенного элемента средним арифметическим/main.py
@@ -1,12 +1,12 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
 # TODO заменить значение пропущенного элемента средним арифметическим
-
+missing_index = 4
 numbers_len = len(numbers)
-before_none = numbers[:4]
-after_none = numbers[5::]
+before_none = numbers[:missing_index]
+after_none = numbers[missing_index+1::]
 numbers_sum = sum(before_none) + sum(after_none)
 numbers_avg = numbers_sum / numbers_len
-numbers[4] = numbers_avg
+numbers[missing_index] = numbers_avg
 
 print("Измененный список:", numbers)
