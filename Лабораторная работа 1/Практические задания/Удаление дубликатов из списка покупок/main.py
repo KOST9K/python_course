09647002aeda57bc684ddb3df60189389ed5f7ff
KOST9K <99390244+KOST9K@users.noreply.github.com>
2023-10-15 12:07:16 +0300

    Delete Лабораторная работа 1/Практические задания directory

--- Лабораторная работа 1/Практические задания/Удаление дубликатов из списка покупок/main.py
+++
@@ -1,8 +0,0 @@
-shopping_list = ["яблоко", "молоко", "хлеб", "яблоко", "масло", "яйца", "молоко"]
-
-# TODO Найдите количество уникальных товаров
-
-unique_items = set(shopping_list)
-unique_items_number = len(unique_items)
-
-print("Количество уникальных товаров:", unique_items_number)
